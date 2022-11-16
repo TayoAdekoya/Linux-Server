d5c67cd6ebf794124d52ac25b4a44232f246094d
TayoAdekoya <114704525+TayoAdekoya@users.noreply.github.com>
2022-11-15 23:32:28 -0700

    update

--- main.tf
+++ main.tf
@@ -135,30 +135,3 @@ output "admin_username" {
 output "vm_fqdn" {
   value = "${azurerm_public_ip.pip.fqdn}"
 }
-
-
-
-resource "azurerm_linux_virtual_machine" "LinuxVM" {
-  name                = var.linux_virtual_machine_name
-  resource_group_name = var.resource_group_name
-  location            = var.resource_group_location
-  size                = var.linux_virtual_machine_size
-  admin_username      = var.admin_username 
-  admin_password      = var.admin_password
-  disable_password_authentication = "false"
-  network_interface_ids = [
-    azurerm_network_interface.NIC.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-}
